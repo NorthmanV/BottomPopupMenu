730e7a7f8be36bd69396428fa9756a32fb59ab8e
Ruslan Akberov <ruslanakberov@gmail.com>
2018-10-07 22:41:35 +0300

    Animations completed

--- BottomPopupMenu/ViewController.swift
+++ BottomPopupMenu/ViewController.swift
@@ -45,54 +45,60 @@ class ViewController: UIViewController {
     
     private lazy var popupView: UIView = {
         let view = UIView()
-        view.backgroundColor = .lightGray
+        view.backgroundColor = .white
         view.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
-        //        view.layer.shadowColor = UIColor.black.cgColor
-        //        view.layer.shadowOpacity = 0.1
-        //        view.layer.shadowRadius = 10
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 0.1
+        view.layer.shadowRadius = 10
         return view
     }()
     
-    //    private lazy var contentImageView: UIImageView = {
-    //        let imageView = UIImageView()
-    //        imageView.image = #imageLiteral(resourceName: "content")
-    //        return imageView
-    //    }()
-    
-    //    private lazy var overlayView: UIView = {
-    //        let view = UIView()
-    //        view.backgroundColor = .black
-    //        view.alpha = 0
-    //        return view
-    //    }()
+    private lazy var contentImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "spb")
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    private lazy var overlayView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.alpha = 0
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 
     
-//    private lazy var closedTitleLabel: UILabel = {
-//        let label = UILabel()
-//        label.text = "Reviews"
-//        label.font = UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.medium)
-//        label.textColor = #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1)
-//        label.textAlignment = .center
-//        return label
-//    }()
+    private lazy var closedTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "St. Petersburg"
+        label.font = UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.medium)
+        label.textColor = #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
-//    private lazy var openTitleLabel: UILabel = {
-//        let label = UILabel()
-//        label.text = "Reviews"
-//        label.font = UIFont.systemFont(ofSize: 24, weight: UIFont.Weight.heavy)
-//        label.textColor = .black
-//        label.textAlignment = .center
-//        label.alpha = 0
-//        label.transform = CGAffineTransform(scaleX: 0.65, y: 0.65).concatenating(CGAffineTransform(translationX: 0, y: -15))
-//        return label
-//    }()
+    private lazy var openTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "St. Petersburg"
+        label.font = UIFont.systemFont(ofSize: 24, weight: UIFont.Weight.heavy)
+        label.textColor = .black
+        label.textAlignment = .center
+        label.alpha = 0
+        label.transform = CGAffineTransform(scaleX: 0.65, y: 0.65).concatenating(CGAffineTransform(translationX: 0, y: -15))
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
-//    private lazy var reviewsImageView: UIImageView = {
-//        let imageView = UIImageView()
-//        imageView.image = #imageLiteral(resourceName: "reviews")
-//        return imageView
-//    }()
-    
+    private lazy var reviewsImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "panorama")
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -102,19 +108,17 @@ class ViewController: UIViewController {
     
     private func layout() {
         
-//        contentImageView.translatesAutoresizingMaskIntoConstraints = false
-//        view.addSubview(contentImageView)
-//        contentImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-//        contentImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-//        contentImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-//        contentImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-//
-//        overlayView.translatesAutoresizingMaskIntoConstraints = false
-//        view.addSubview(overlayView)
-//        overlayView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-//        overlayView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-//        overlayView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-//        overlayView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        view.addSubview(contentImageView)
+        contentImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        contentImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        contentImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        contentImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+
+        view.addSubview(overlayView)
+        overlayView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        overlayView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        overlayView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        overlayView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         
         popupView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(popupView)
@@ -124,110 +128,21 @@ class ViewController: UIViewController {
         bottomConstraint.isActive = true
         popupView.heightAnchor.constraint(equalToConstant: 500).isActive = true
         
-//        closedTitleLabel.translatesAutoresizingMaskIntoConstraints = false
-//        popupView.addSubview(closedTitleLabel)
-//        closedTitleLabel.leadingAnchor.constraint(equalTo: popupView.leadingAnchor).isActive = true
-//        closedTitleLabel.trailingAnchor.constraint(equalTo: popupView.trailingAnchor).isActive = true
-//        closedTitleLabel.topAnchor.constraint(equalTo: popupView.topAnchor, constant: 20).isActive = true
-//
-//        openTitleLabel.translatesAutoresizingMaskIntoConstraints = false
-//        popupView.addSubview(openTitleLabel)
-//        openTitleLabel.leadingAnchor.constraint(equalTo: popupView.leadingAnchor).isActive = true
-//        openTitleLabel.trailingAnchor.constraint(equalTo: popupView.trailingAnchor).isActive = true
-//        openTitleLabel.topAnchor.constraint(equalTo: popupView.topAnchor, constant: 30).isActive = true
-//
-//        reviewsImageView.translatesAutoresizingMaskIntoConstraints = false
-//        popupView.addSubview(reviewsImageView)
-//        reviewsImageView.leadingAnchor.constraint(equalTo: popupView.leadingAnchor).isActive = true
-//        reviewsImageView.trailingAnchor.constraint(equalTo: popupView.trailingAnchor).isActive = true
-//        reviewsImageView.bottomAnchor.constraint(equalTo: popupView.bottomAnchor).isActive = true
-//        reviewsImageView.heightAnchor.constraint(equalToConstant: 428).isActive = true
-        
-    }
-    
-    /// Animates the transition, if the animation is not already running.
-    private func animateTransitionIfNeeded(to state: State, duration: TimeInterval) {
-        
-        // ensure that the animators array is empty (which implies new animations need to be created)
-        guard runningAnimators.isEmpty else { return }
-        
-        // an animator for the transition
-        let transitionAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1, animations: {
-            switch state {
-            case .opened:
-                self.bottomConstraint.constant = 0
-                self.popupView.layer.cornerRadius = 20
-//                self.overlayView.alpha = 0.5
-//                self.closedTitleLabel.transform = CGAffineTransform(scaleX: 1.6, y: 1.6).concatenating(CGAffineTransform(translationX: 0, y: 15))
-//                self.openTitleLabel.transform = .identity
-            case .closed:
-                self.bottomConstraint.constant = self.popupOffset
-                self.popupView.layer.cornerRadius = 0
-//                self.overlayView.alpha = 0
-//                self.closedTitleLabel.transform = .identity
-//                self.openTitleLabel.transform = CGAffineTransform(scaleX: 0.65, y: 0.65).concatenating(CGAffineTransform(translationX: 0, y: -15))
-            }
-            self.view.layoutIfNeeded()
-        })
-        
-        // the transition completion block
-        transitionAnimator.addCompletion { position in
-            
-            // update the state
-            switch position {
-            case .start:
-                self.currentState = state.opposite
-            case .end:
-                self.currentState = state
-            case .current:
-                ()
-            }
-            
-            // manually reset the constraint positions
-            switch self.currentState {
-            case .opened:
-                self.bottomConstraint.constant = 0
-            case .closed:
-                self.bottomConstraint.constant = self.popupOffset
-            }
-            
-            // remove all running animators
-            self.runningAnimators.removeAll()
-            
-        }
-        
-//        // an animator for the title that is transitioning into view
-//        let inTitleAnimator = UIViewPropertyAnimator(duration: duration, curve: .easeIn, animations: {
-//            switch state {
-//            case .opened:
-//                self.openTitleLabel.alpha = 1
-//            case .closed:
-//                self.closedTitleLabel.alpha = 1
-//            }
-//        })
-//        inTitleAnimator.scrubsLinearly = false
-//
-//        // an animator for the title that is transitioning out of view
-//        let outTitleAnimator = UIViewPropertyAnimator(duration: duration, curve: .easeOut, animations: {
-//            switch state {
-//            case .opened:
-//                self.closedTitleLabel.alpha = 0
-//            case .closed:
-//                self.openTitleLabel.alpha = 0
-//            }
-//        })
-//        outTitleAnimator.scrubsLinearly = false
-        
-        // start all animators
-        transitionAnimator.startAnimation()
-//        inTitleAnimator.startAnimation()
-//        outTitleAnimator.startAnimation()
-        
-        // keep track of all running animators
-        runningAnimators.append(transitionAnimator)
-//        runningAnimators.append(inTitleAnimator)
-//        runningAnimators.append(outTitleAnimator)
-        
+        popupView.addSubview(closedTitleLabel)
+        closedTitleLabel.leadingAnchor.constraint(equalTo: popupView.leadingAnchor).isActive = true
+        closedTitleLabel.trailingAnchor.constraint(equalTo: popupView.trailingAnchor).isActive = true
+        closedTitleLabel.topAnchor.constraint(equalTo: popupView.topAnchor, constant: 20).isActive = true
+
+        popupView.addSubview(openTitleLabel)
+        openTitleLabel.leadingAnchor.constraint(equalTo: popupView.leadingAnchor).isActive = true
+        openTitleLabel.trailingAnchor.constraint(equalTo: popupView.trailingAnchor).isActive = true
+        openTitleLabel.topAnchor.constraint(equalTo: popupView.topAnchor, constant: 30).isActive = true
+
+        popupView.addSubview(reviewsImageView)
+        reviewsImageView.leadingAnchor.constraint(equalTo: popupView.leadingAnchor).isActive = true
+        reviewsImageView.trailingAnchor.constraint(equalTo: popupView.trailingAnchor).isActive = true
+        reviewsImageView.bottomAnchor.constraint(equalTo: popupView.bottomAnchor).isActive = true
+        reviewsImageView.heightAnchor.constraint(equalToConstant: 428).isActive = true
     }
     
     @objc private func popupViewPanned(recognizer: UIPanGestureRecognizer) {
@@ -245,7 +160,6 @@ class ViewController: UIViewController {
             
         case .changed:
             
-            // variable setup
             let translation = recognizer.translation(in: popupView)
             var fraction = -translation.y / popupOffset
             
@@ -260,7 +174,6 @@ class ViewController: UIViewController {
             
         case .ended:
             
-            // variable setup
             let yVelocity = recognizer.velocity(in: popupView).y
             let shouldClose = yVelocity > 0
             
@@ -286,6 +199,85 @@ class ViewController: UIViewController {
         default:
             ()
         }
+    }
+    
+    /// Animates the transition, if the animation is not already running.
+    private func animateTransitionIfNeeded(to state: State, duration: TimeInterval) {
+        
+        // ensure that the animators array is empty (which implies new animations need to be created)
+        guard runningAnimators.isEmpty else { return }
+        
+        // an animator for the transition
+        let transitionAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1, animations: {
+            switch state {
+            case .opened:
+                self.bottomConstraint.constant = 0
+                self.popupView.layer.cornerRadius = 20
+                self.overlayView.alpha = 0.5
+                self.closedTitleLabel.transform = CGAffineTransform(scaleX: 1.6, y: 1.6).concatenating(CGAffineTransform(translationX: 0, y: 15))
+                self.openTitleLabel.transform = .identity
+            case .closed:
+                self.bottomConstraint.constant = self.popupOffset
+                self.popupView.layer.cornerRadius = 0
+                self.overlayView.alpha = 0
+                self.closedTitleLabel.transform = .identity
+                self.openTitleLabel.transform = CGAffineTransform(scaleX: 0.65, y: 0.65).concatenating(CGAffineTransform(translationX: 0, y: -15))
+            }
+            self.view.layoutIfNeeded()
+        })
+        
+        // the transition completion block
+        transitionAnimator.addCompletion { position in
+            
+            // update the state
+            switch position {
+            case .start: self.currentState = state.opposite
+            case .end: self.currentState = state
+            case .current: ()
+            }
+            
+            // manually reset the constraint positions
+            switch self.currentState {
+            case .opened: self.bottomConstraint.constant = 0
+            case .closed: self.bottomConstraint.constant = self.popupOffset
+            }
+            
+            // remove all running animators
+            self.runningAnimators.removeAll()
+        }
+        
+        // an animator for the title that is transitioning into view
+        let inTitleAnimator = UIViewPropertyAnimator(duration: duration, curve: .easeIn, animations: {
+            switch state {
+            case .opened:
+                self.openTitleLabel.alpha = 1
+            case .closed:
+                self.closedTitleLabel.alpha = 1
+            }
+        })
+        inTitleAnimator.scrubsLinearly = false
+
+        // an animator for the title that is transitioning out of view
+        let outTitleAnimator = UIViewPropertyAnimator(duration: duration, curve: .easeOut, animations: {
+            switch state {
+            case .opened:
+                self.closedTitleLabel.alpha = 0
+            case .closed:
+                self.openTitleLabel.alpha = 0
+            }
+        })
+        outTitleAnimator.scrubsLinearly = false
+        
+        // start all animators
+        transitionAnimator.startAnimation()
+        inTitleAnimator.startAnimation()
+        outTitleAnimator.startAnimation()
+        
+        // keep track of all running animators
+        runningAnimators.append(transitionAnimator)
+        runningAnimators.append(inTitleAnimator)
+        runningAnimators.append(outTitleAnimator)
+        
     }
     
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+# Bottom popup menu
+Inspired by tutorial from [here](http://www.swiftkickmobile.com/building-better-app-animations-swift-uiviewpropertyanimator/).
+![Screenshot](screenshot1.png)
+![Screenshot](screenshot2.png)
+
